5ebc261da9ba332995cecfccfa846a8fc66ee37f
MesQue1041 <dovebookt@gmail.com>
2024-09-23 20:05:23 +0530

    Login code done

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet"
 import morgan from "morgan"
 import path from "path";
 import { fileURLToPath } from 'url';
+import authRoutes from "./routes/auth.js"
 import { register } from "./controllers/auth.js"
 
 //Middleware
@@ -50,6 +51,10 @@ const upload = multer ({ storage });
 
 // Routes with files
 app.post("auth/register", upload.single("picture"), register)
+
+//Routes
+app.use("/auth", authRoutes)
+
 
 // Database
 const PORT = process.env.PORT || 6001;
